02366b5004c31b81a7f16f87b731d0a4c3e4298d
Nicolas <nicolas@desktop>
2021-03-15 20:01:17 -0300

    Recepcion archivos

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Torno Comm' });
+});
+
+router.get('/received', function(req, res, next) {
+  res.render('received', { title: 'Archivos recibidos' });
 });
 
 module.exports = router;
--- routes/serial.js
+++ routes/serial.js
@@ -1,19 +1,9 @@
 var express = require('express');
 const SerialPort = require('serialport');
+const ReadLine = SerialPort.parsers.Readline
 var router = express.Router();
 
 router.post('/send', function(req, res, next) {
-  console.info({
-    baudRate: req.body['baud_rate'],
-    parity: req.body['parity'],
-    dataBits: req.body['data_bits'],
-    stopBits: req.body['stop_bits'],
-    rtscts: req.body['flow_control'] == "rts/cts",
-    xany: req.body['flow_control'] == 'xon/off',
-    xon: req.body['flow_control'] == 'xon/off',
-    xoff:  req.body['flow_control'] == 'xon/off'
-  })  
-
   const serial = new SerialPort('/dev/ttyUSB0', {
     baudRate: req.body['baud_rate'],
     parity: req.body['parity'],
@@ -39,6 +29,41 @@ router.post('/send', function(req, res, next) {
       }
       serial.close()
     })
+  })
+});
+
+router.get('/receive', function(req, res, next) {
+  const serial = new SerialPort('/dev/ttyUSB0', {
+    baudRate: req.query['baud_rate'],
+    parity: req.query['parity'],
+    dataBits: req.query['data_bits'],
+    stopBits: req.query['stop_bits'],
+    rtscts: req.query['flow_control'] == "rts/cts",
+    xany: req.query['flow_control'] == 'xon/off',
+    xon: req.query['flow_control'] == 'xon/off',
+    xoff:  req.query['flow_control'] == 'xon/off'
+  }, (e)=> {
+    if(e) {
+      console.error(e)
+      res.json({status: 'Error al conectar', message: e})
+    }
+  })
+  serial.on('open', ()=> {
+    var file_start_flag = false;
+    var parser = new ReadLine();
+    var buffer = [];
+    serial.pipe(parser);
+    parser.on('data', (data) => {
+      buffer.push(data);
+      if (data == '%') {
+        if(file_start_flag) {
+          serial.close();
+          res.send(buffer.join('\n'));
+        } else {
+          file_start_flag = true;
+        }
+      }
+    });
   })
 });
 
