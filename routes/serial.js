e02ae3de446f4564595cc6a24e2108fdee98658f
Nicolas <ubuntu20@thinkpad>
2021-07-26 19:41:55 -0300

    Change logic

--- routes/serial.js
+++ routes/serial.js
@@ -3,7 +3,7 @@ const SerialPort = require('serialport');
 const ReadLine = SerialPort.parsers.Readline
 var router = express.Router();
 
-router.post('/send', function(req, res, next) {
+router.post('/send', function (req, res, next) {
   const serial = new SerialPort('/dev/ttyUSB0', {
     baudRate: parseInt(req.body['baud_rate']),
     parity: req.body['parity'],
@@ -12,32 +12,27 @@ router.post('/send', function(req, res, next) {
     rtscts: req.body['flow_control'] == "rts/cts",
     xany: req.body['flow_control'] == 'xon/off',
     xon: req.body['flow_control'] == 'xon/off',
-    xoff:  req.body['flow_control'] == 'xon/off'
-  }, (e)=> {
-    if(e) {
+    xoff: req.body['flow_control'] == 'xon/off'
+  }, (e) => {
+    if (e) {
       console.error(e)
-      res.json({status: 'Error al conectar. Espere 10 segundos y vuelva a intentar', message: e})
+      res.json({ status: 'Error al conectar. Espere 10 segundos y vuelva a intentar', message: e })
     }
   })
-  serial.on('open', ()=> {
+  serial.on('open', () => {
     var timeout = setTimeout(() => {
-      serial.drain(() => serial.close());
+      serial.close();
       res.json('Error al enviar datos. Timeout');
-    }, req.body['file'].length / req.body['baud_rate'] * 1000)
-    serial.write(req.body['file'], (err)=> {
-      if(err) {
-        console.error(e)
-        res.json({status: 'Error al enviar', message: e })
-      } else {
-        res.json({ status: "OK", message: "Archivo enviado correctamente" })
-      }
-      clearTimeout(timeout);
-      serial.close()
-    })
+    }, req.body['file'].length / req.body['baud_rate'] * 1000);
+
+    serial.write(req.body['file']);
+    serial.drain();
+    clearTimeout(timeout);
+    serial.close();
   })
 });
 
-router.get('/receive', function(req, res, next) {
+router.get('/receive', function (req, res, next) {
   const serial = new SerialPort('/dev/ttyUSB0', {
     baudRate: parseInt(req.query['baud_rate']),
     parity: req.query['parity'],
@@ -46,19 +41,23 @@ router.get('/receive', function(req, res, next) {
     rtscts: req.query['flow_control'] == "rts/cts",
     xany: req.query['flow_control'] == 'xon/off',
     xon: req.query['flow_control'] == 'xon/off',
-    xoff:  req.query['flow_control'] == 'xon/off'
-  }, (e)=> {
-    if(e) {
+    xoff: req.query['flow_control'] == 'xon/off'
+  }, (e) => {
+    if (e) {
       console.error(e)
-      res.json({status: 'Error al conectar. Espere 10 segundos y vuelva a intentar', message: e})
+      res.json({ status: 'Error al conectar. Espere 10 segundos y vuelva a intentar', message: e })
     }
   })
-  serial.on('open', ()=> {
+  serial.on('open', () => {
+    serial.flush();
     var timeout = 0;
-    const set_timeout = () => { timeout = setTimeout(() => {
-      serial.flush(() => serial.close());
-      res.json({status: 'Error al recibir datos. Timeout'});
-    }, 10000) };
+    const set_timeout = () => {
+      timeout = setTimeout(() => {
+        serial.flush();
+        serial.close();
+        res.json({ status: 'Error al recibir datos. Timeout', recv: buffer.join('\n') });
+      }, 10000)
+    };
     set_timeout();
     var file_start_flag = false;
     var parser = new ReadLine();
@@ -67,8 +66,8 @@ router.get('/receive', function(req, res, next) {
     parser.on('data', (data) => {
       clearTimeout(timeout);
       buffer.push(data);
-      if (data == '%') {
-        if(file_start_flag) {
+      if (data.trim() == '%') {
+        if (file_start_flag) {
           serial.flush();
           serial.close();
           res.send(buffer.join('\n'));
